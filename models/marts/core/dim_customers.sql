316ceaa75cfabdb81262bed21b3642ffa752b69c
dsmdavidIL <61737063+dsmdavidIL@users.noreply.github.com>
2023-10-03 14:24:29 +0100

    update customer

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -9,18 +9,32 @@ with customer as (
 
 ),
 
+nation as (
+    select * from {{ ref('stg_tpch_nations') }}
+),
+
+region as (
+    select * from {{ ref('stg_tpch_regions') }}
+)
+
 final as (
     select    
-        customer_key,
-        name,
-        address, 
-        nation_key,
-        phone_number,
-        account_balance,
-        market_segment,
-        comment
+        c.customer_key,
+        c.name,
+        c.address, 
+        c.nation_key,
+        c.phone_number,
+        c.account_balance,
+        c.market_segment,
+        c.comment,
+        n.name as nation_name,
+        r.name as region 
     from
-        customer
+        customer as c 
+    left join nation as n 
+        on c.nation_key = n.nation_key
+    left join region as r 
+        on n.region_key as r.region_key
 )
 select *
 from final  
